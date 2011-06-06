911554764e1b1fb84aea7ede67bdb167086e19de
Jordan Adams <jordan_callum@live.co.uk>
2011-06-06 18:37:59 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="description" content="  " />
+		<meta name="generator" content="Studio 3 http://aptana.com/" />
+		<meta name="author" content="Jordan Adams" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+		<title>Forrst Snap Grid</title>
+		<link href="css/reset.css" rel="stylesheet" />
+		<link href="css/style.css" rel="stylesheet" />
+		<script src="http://code.jquery.com/jquery.min.js"></script>
+		<script src="js/main.js"></script>
+		<script>
+			
+			$(document).ready(function() {
+				
+				/* Do Stuff */
+				
+			});
+			
+		</script>
+	</head>
+
+	<body>
+		
+		<div id="glow"></div>
+		
+		<h1>Forrst Snap Grid</h1>
+		
+		<label>User: </label><input type="text" value="JordanAdams" id="username"></input>
+		
+		<div id="snapGrid"></div>
+		
+		<footer>by, Jordan Adams<br />@JordanCallumA</footer>
+		
+	</body>
+</html>
